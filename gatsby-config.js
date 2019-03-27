76fc3abf6287b765ff905bcf0956fce229c7fc79
Helder S Ribeiro <hsribei.pub@gmail.com>
2019-03-27 09:11:06 -0400

    Add syntax highlighting and other remark plugins
    Basically:
    - Autolink headers
    - Handle big images
    - Link to online repl with contents of example file

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,39 @@ module.exports = {
         defaultLayouts: {
           default: require.resolve('./src/components/layout.js'),
         },
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              classPrefix: 'language-',
+            },
+          },
+          { resolve: 'gatsby-remark-copy-linked-files', options: {} },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-code-repls',
+            options: {
+              externals: [
+                'https://cdnjs.cloudflare.com/ajax/libs/react/16.2.0/umd/react.production.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.2.0/umd/react-dom.production.min.js',
+              ],
+              dependencies: ['d3'],
+              codesandbox: ['react', 'react-dom', 'd3'],
+              html: '<div id="root"></div>',
+              directory: `${__dirname}/examples/`,
+              target: '_blank',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-autolink-headers',
+            options: {},
+          },
+        ],
       },
     },
     'gatsby-plugin-nprogress',
